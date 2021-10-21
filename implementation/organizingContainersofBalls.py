75461df921ea845cd49d50b858db9d389d33f6c4
Jonathan Prieto <jonathanprietom@gmail.com>
2021-10-20 19:13:03 -0500

    add solution to Organizing Containers of Balls problem

---
+++ implementation/organizingContainersofBalls.py
@@ -0,0 +1,42 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'organizingContainers' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts 2D_INTEGER_ARRAY container as parameter.
+#
+
+
+def organizingContainers(container):
+    # Write your code here
+    rows = sorted([sum(x) for x in container])
+    columns = sorted([sum(x) for x in zip(*container)])
+    result = "Possible" if rows == columns else "Impossible"
+    return result
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    q = int(input().strip())
+
+    for q_itr in range(q):
+        n = int(input().strip())
+
+        container = []
+
+        for _ in range(n):
+            container.append(list(map(int, input().rstrip().split())))
+
+        result = organizingContainers(container)
+
+        fptr.write(result + '\n')
+
+    fptr.close()
